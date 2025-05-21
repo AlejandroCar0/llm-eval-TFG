9b2b3bd8e11ff05934df9e4fe417ab4ae6bc2b19
AlejandroCar0 <alejandro.caro@alumnos.upm.es>
2025-05-21 18:10:02 +0200

    Fixed configurations.sh

--- configurations.sh
+++ configurations.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-execution_dir=$(realpath $(dirname $0))
-target_name=ollama-linux-amd64
+WORK_DIR=$(realpath $(dirname $0))
 password=$1
-
-export WORK_DIR=$execution_dir
 
 echo $password | sudo -S apt update
 echo $password | sudo -S apt upgrade
@@ -17,15 +14,19 @@ curl
 echo $password | sudo -S apt clean && rm -rf /var/lib/apt/list/*
 
 #Download and configure ollama
-mkdir $WORK_DIR/ollama
-export OLLAMA_PATH=$WORK_DIR/ollama
-curl -L https://github.com/ollama/ollama/releases/download/v0.5.11/ollama-linux-amd64.tgz -o "$OLLAMA_PATH/$target_name.tgz"
-gunzip $OLLAMA_PATH/$target_name.tgz 
-tar xf $OLLAMA_PATH/$target_name.tar -C $OLLAMA_PATH
-ls  $OLLAMA_PATH | grep -v -e "bin" -e "lib" | xargs rm -rf
+OLLAMA_PATH=$WORK_DIR/ollama
+mkdir -p $OLLAMA_PATH
+
+curl -L https://github.com/ollama/ollama/releases/download/v0.5.11/ollama-linux-amd64.tgz -o $OLLAMA_PATH/ollama.tgz
+tar xzf $OLLAMA_PATH/ollama.tgz -C $OLLAMA_PATH
+ls $OLLAMA_PATH | grep -v -E "(^bin$|^lib$)" | xargs -I{} rm -rf $OLLAMA_PATH/{}
+#ls  $OLLAMA_PATH | grep -v -e "bin" -e "lib" | xargs rm -rf
 
 #Download and configure the nodeExporter for prometheus
-
-curl -L https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz -o "$WORK_DIR/nodeExporter.tar.gz"
-tar xf $WORK_DIR/nodeExporter.tar.gz -C $WORK_DIR/node_exporter
-rm -rf $WORK_DIR/nodeExporter.tar.gz
+EXPORTER_PATH=$WORK_DIR/node_exporter
+mkdir -p $EXPORTER_PATH
+curl -o $EXPORTER_PATH/nodeExporter.tar.gz -L https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar xf $EXPORTER_PATH/nodeExporter.tar.gz -C $EXPORTER_PATH
+mv $EXPORTER_PATH/node_exporter-*/* $EXPORTER_PATH
+ls $EXPORTER_PATH | grep -v -e "^node_exporter$" | xargs -I{} rm -rf $EXPORTER_PATH/{}
+#rm -rf $WORK_DIR/nodeExporter.tar.gz
